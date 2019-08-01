8e65f571e71549fd4d4119ac171b5898ad899277
Darxoon <Jakob.priemer@gmail.com>
2019-08-01 13:03:17 +0200

    Rearrange sounds.ts to support sound arrays

--- src/ts/interaction/sounds.ts
+++ src/ts/interaction/sounds.ts
@@ -49,49 +49,94 @@ export function loadAllSounds() {
 	console.log(soundLib)
 }
 
-export function play(sound: Sound, gain = 1, pan = 0, sinPan = false) {
+function combineAudioBuffers(audioBuffers: AudioBuffer[]) {
+	// iterate through channels
+	const minChannelAmount = audioBuffers.reduce((previousValue, currentValue) => { console.log(currentValue); return Math.min(previousValue, currentValue.numberOfChannels) }, 100),
+		  combinedDuration = audioBuffers.reduce((previousValue, currentValue) => { return previousValue + currentValue.length }, 0),
+		  newBuffer		 = context.createBuffer(minChannelAmount, combinedDuration, audioBuffers[0].sampleRate)
+	for (let i = 0; i < minChannelAmount; i++) {
+
+		const channelData = newBuffer.getChannelData(i)
+
+		// iterate through audioBuffers
+		for (let j = 0, channelPosition = 0; j < audioBuffers.length; j++) {
+			channelData.set(audioBuffers[j].getChannelData(i), channelPosition)
+			channelPosition += audioBuffers[j].length
+		}
+
+	}
+
+	return newBuffer
+}
+
+function textToSpeech(sound: {type: 'tts', voice: string, text: string}): AudioBuffer {
+	console.log('text to speech:', sound)
+	return soundLib.tts_placeholder
+}
+
+function playBuffer(audioBuffer: AudioBuffer, gain = 1, pan = 0, sinPanning = false) {
+
+	const source = context.createBufferSource()
+	source.buffer = audioBuffer
+
+	const gainNode = context.createGain()
+	gainNode.gain.value = gain
+	source.connect(gainNode)
+
+	const pannerNode = context.createStereoPanner()
+	pannerNode.pan.value = pan
+	gainNode.connect(pannerNode)
+
+	pannerNode.connect(context.destination)
+	source.start()
+
+
+	if (sinPanning) {
+
+		let interval: NodeJS.Timeout
+		let count = 0
+
+		interval = setInterval(() => {
+			pannerNode.pan.value = Math.sin(pan)
+			pan += 0.01
+			count++
+
+		}, 1)
+
+		source.addEventListener('ended', () => {
+			clearInterval(interval)
+		})
+	}
+
+}
+
+export function play(sound: Sound | Sound[], gain = 1, pan = 0, sinPan = false) {
+
+	// if it's an array, combine it and play it
+	if(sound instanceof Array) {
+		return playBuffer(combineAudioBuffers(sound.map(value => {
+			if(typeof value === 'string')
+				value = { type: 'file', name: value }
+
+			if(value.type === 'tts')
+				return textToSpeech(value)
+			else
+				return soundLib[value.name]
+		})), gain, pan, sinPan)
+	}
+
+	// if it's a string, convert it to an object
 	if(typeof sound === 'string')
 		sound = { type: 'file', name: sound }
+
+	// if it's a file, play item from soundLib
 	if(sound.type === 'file') {
 		if(soundLib[sound.name]) {
-			// play file
-			const source = context.createBufferSource()
-			source.buffer = soundLib[sound.name]
-
-
-			const gainNode = context.createGain()
-			gainNode.gain.value = gain
-			source.connect(gainNode)
-
-			const pannerNode = context.createStereoPanner()
-			pannerNode.pan.value = pan
-			gainNode.connect(pannerNode)
-
-			pannerNode.connect(context.destination)
-			source.start()
-
-
-			if (sinPan) {
-
-				let interval: NodeJS.Timeout
-				let count = 0
-
-				interval = setInterval(() => {
-					pannerNode.pan.value = Math.sin(pan)
-					pan += 0.01
-					count++
-
-				}, 1)
-
-				source.addEventListener('ended', () => {
-					clearInterval(interval)
-				})
-			}
+			playBuffer(soundLib[sound.name])
 		} else
 			console.error(`Sound called '${sound.name} doesn't exist`)
-	} else {
-		// text to speech
-		// temporarily, text to speech will play the placeholder file
-		play('tts_placeholder')
-	}
+
+	} else
+		// play text-to-speech
+		playBuffer(textToSpeech(sound), gain, pan, sinPan)
 }
